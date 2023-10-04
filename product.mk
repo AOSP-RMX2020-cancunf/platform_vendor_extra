fa360fcf6688cd3ca29da3b5d10a278cd4976ac0
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-04 23:28:58 +0530

    product: Add GMS flags
    Change-Id: I34db6b293d04809b38755923d63a5012f516c7a0

--- product.mk
+++ product.mk
@@ -4,10 +4,17 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
+# GMS
+ifeq ($(WITH_GMS), true)
+    $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+endif
+
 # App
-PRODUCT_PACKAGES += \
+ifneq ($(WITH_GMS), true)
+    PRODUCT_PACKAGES += \
 	ThemePicker \
 	messaging
+endif
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
