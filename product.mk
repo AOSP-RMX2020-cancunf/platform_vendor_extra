e553a9e1a4d0a8bf455935f85e7e4c9589465f01
Sarthak Roy <sarthakroy2002@gmail.com>
2024-09-04 17:14:30 +0530

    extra: Switch to partner_gms

--- product.mk
+++ product.mk
@@ -6,7 +6,7 @@
 
 # GMS
 ifeq ($(WITH_GMS), true)
-    $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+    $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 endif
 
 # APN
